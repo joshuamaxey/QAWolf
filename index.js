c229e81584e1d19e7f66d060c8956425215a4078
joshuamaxey <joshua.maxey.leija@icloud.com>
2025-07-11 17:32:19 -0500

    Added browser.close() to index.js as well

--- index.js
+++ index.js
@@ -4,59 +4,64 @@ import { chromium } from "playwright";
 async function sortHackerNewsArticles() {
   // launch browser
   const browser = await chromium.launch({ headless: false });
-  const context = await browser.newContext();
-  const page = await context.newPage();
 
-  // go to Hacker News
-  await page.goto("https://news.ycombinator.com/newest");
+  try {
+    const context = await browser.newContext();
+    const page = await context.newPage();
 
-  console.log("Grabbing the titles and IDs of the first 100 Hacker News articles...");
-  // First, we will grab the first 100 articles (their IDs and titles)
-  const articles = [];
+    // go to Hacker News
+    await page.goto("https://news.ycombinator.com/newest");
 
-  while (articles.length < 100) {
-    const pageArticles = await page.$$eval('tr.athing.submission', rows => {
-      return rows.map(row => {
-        const id = row.id;
-        const link = row.querySelector('.titleline > a');
-        const title = link?.textContent?.trim(); // get rid of any whitespace
+    console.log("Grabbing the titles and IDs of the first 100 Hacker News articles...");
+    // First, we will grab the first 100 articles (their IDs and titles)
+    const articles = [];
 
-        if (id && title) {
-          return { id, title };
-        }
-      })
-    });
+    while (articles.length < 100) {
+      const pageArticles = await page.$$eval('tr.athing.submission', rows => {
+        return rows.map(row => {
+          const id = row.id;
+          const link = row.querySelector('.titleline > a');
+          const title = link?.textContent?.trim(); // get rid of any whitespace
 
-    articles.push(...pageArticles);
+          if (id && title) {
+            return { id, title };
+          }
+        })
+      });
 
-    const moreLink = await page.$('a.morelink');
+      articles.push(...pageArticles);
 
-    await Promise.all([
-      moreLink.click()
-    ]);
+      const moreLink = await page.$('a.morelink');
+
+      await Promise.all([
+        moreLink.click()
+      ]);
+    }
+
+    // Take only the first 100 articles
+    let hundredArticles = articles.slice(0, 100);
+
+    console.log("Fetching the articles by their ID, then accessing their 'time' attribute...")
+    // Now lets use the Hacker News API to fetch each article by its ID so that we can access its 'time' attribute and add it to article since that's how we can verify the order
+    for (let article of hundredArticles) {
+      const response = await fetch(`https://hacker-news.firebaseio.com/v0/item/${article.id}.json`)
+      const articleData = await response.json();
+      article.time = articleData?.time;
+    }
+
+    console.log("Comparing 'time' attributes to verify sorting order of articles...")
+    // Now we can use Array.every() to check the order of the articles
+    const isSorted = hundredArticles.every((article, i, arr) => {
+      if (i === 0) return true; // ignore the first article
+      return arr[i - 1].time >= article.time; // make sure the current article is 'older' than the previous one
+    })
+
+    // console.log(hundredArticles);
+    console.log("Number of articles:", hundredArticles.length);
+    console.log("Are the articles sorted correctly by creation date?", isSorted);
+  } finally {
+    await browser.close()
   }
-
-  // Take only the first 100 articles
-  let hundredArticles = articles.slice(0, 100);
-
-  console.log("Fetching the articles by their ID, then accessing their 'time' attribute...")
-  // Now lets use the Hacker News API to fetch each article by its ID so that we can access its 'time' attribute and add it to article since that's how we can verify the order
-  for (let article of hundredArticles) {
-    const response = await fetch(`https://hacker-news.firebaseio.com/v0/item/${article.id}.json`)
-    const articleData = await response.json();
-    article.time = articleData?.time;
-  }
-
-  console.log("Comparing 'time' attributes to verify sorting order of articles...")
-  // Now we can use Array.every() to check the order of the articles
-  const isSorted = hundredArticles.every((article, i, arr) => {
-    if (i === 0) return true; // ignore the first article
-    return arr[i - 1].time >= article.time; // make sure the current article is 'older' than the previous one
-  })
-
-  // console.log(hundredArticles);
-  console.log("Number of articles:", hundredArticles.length);
-  console.log("Are the articles sorted correctly by creation date?", isSorted);
 }
 
 (async () => {
