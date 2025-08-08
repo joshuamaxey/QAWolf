19d4748fa920658fa56b11b5415fdccb7f5f608a
joshuamaxey <joshua.maxey.leija@icloud.com>
2025-08-08 13:55:36 -0500

    Wrote and tested all 7 test blocks within hackerNewsAuth.spec.js test suite, completing QA Wolf takehome assessment.

--- tests/hackerNewsAuth.spec.js
+++ tests/hackerNewsAuth.spec.js
@@ -1,5 +1,5 @@
 import { test, expect } from "@playwright/test";
-import { goToHackerNews, goToHackerNewsLogin, goToProfilePageHN, loginHackerNews, logoutHackerNews } from "./helpers/authHelpers";
+import { goToHackerNews, goToHackerNewsLogin, goToProfilePageHN, loginBadCredentialsHN, loginHackerNews, logoutHackerNews, updateAboutSectionHN } from "./helpers/authHelpers";
 
 // Verify that "Hacker News" is visible on the homepage
 test('"Hacker News" visible on homepage', async ({ page }) => {
@@ -9,12 +9,14 @@ test('"Hacker News" visible on homepage', async ({ page }) => {
 
 // Verify that 'Login' button is visible on login/signup page
 test('"Login" is visible on login/signup page', async ({ page }) => {
+    await goToHackerNews(page);
     await goToHackerNewsLogin(page);
     await expect(page.getByRole('button', { name: 'login' })).toBeVisible();
 })
 
 // Verify that username is visible on homepage after logging in
-test('"<username>" is visible on homepage after login', async ({ page }) => {
+test('"<username>" is visible on homepage after successful login', async ({ page }) => {
+    await goToHackerNews(page);
     await goToHackerNewsLogin(page);
     await loginHackerNews(page);
     await expect(page.getByText("blaiddmaxey")).toBeVisible();
@@ -23,6 +25,7 @@ test('"<username>" is visible on homepage after login', async ({ page }) => {
 // Verify that 'login' is visible on the page after logging out
 // Then verify that <username> is not visible on the page
 test('After logout, "login" is visible and username is not', async ({ page }) => {
+    await goToHackerNews(page);
     await goToHackerNewsLogin(page);
     await loginHackerNews(page);
     await logoutHackerNews(page);
@@ -30,10 +33,32 @@ test('After logout, "login" is visible and username is not', async ({ page }) =>
     await expect(page.getByText("blaiddmaxey")).not.toBeVisible();
 })
 
-test('User profile page shows "change password" link and "update" button', async ({ page }) => {
+// Verify that the user's own profile page shows 'change password' link and 'update' button
+test('Profile page shows "change password" and "update" button', async ({ page }) => {
+    await goToHackerNews(page);
     await goToHackerNewsLogin(page);
     await loginHackerNews(page);
     await goToProfilePageHN(page);
     await expect(page.getByRole('link', { name: 'change password' })).toBeVisible();
     await expect(page.getByRole('button', { name: 'update' })).toBeVisible();
+})
+
+
+// Verify that user's 'about' section reflects changes after it's been updated
+test('New "about" text visible after updating about section on profile', async ({ page }) => {
+    await goToHackerNews(page);
+    await goToHackerNewsLogin(page);
+    await loginHackerNews(page);
+    await goToProfilePageHN(page);
+    const newText = await updateAboutSectionHN(page);
+    // Make sure the new "about" text is visible on the profile page following update
+    await expect(page.getByText(newText)).toBeVisible();
+})
+
+// Verify that 'Bad login' is visible on login page after trying to log in with invalid username
+test('"Bad login" is visible after trying to log in with invalid username', async ({ page }) => {
+    await goToHackerNews(page);
+    await goToHackerNewsLogin(page);
+    await loginBadCredentialsHN(page);
+    await expect(page.getByText("Bad login.")).toBeVisible();
 })
--- tests/helpers/authHelpers.js
+++ tests/helpers/authHelpers.js
@@ -1,4 +1,5 @@
 // ! Helper functions for testing the Hacker News authentication features
+import uniqueUsernameGenerator from "unique-username-generator";
 
 // Open the browser, navigate to the website
 export async function goToHackerNews(page) {
@@ -7,14 +8,11 @@ export async function goToHackerNews(page) {
 
 // Navigate to the login/signup page
 export async function goToHackerNewsLogin(page) {
-    await goToHackerNews(page);
     await page.getByRole('link', { name: 'login' }).click();
 }
 
 // Log in to Hacker News
 export async function loginHackerNews(page) {
-    await goToHackerNewsLogin(page);
-
     const loginForm = page.locator('form', {
         has: page.getByRole('button', { name: 'login' }),
     });
@@ -33,4 +31,27 @@ export async function logoutHackerNews(page) {
 // Go to profile page
 export async function goToProfilePageHN(page) {
     await page.locator('#me').click();
+}
+
+// Update 'about' section
+export async function updateAboutSectionHN(page) {
+    const newAboutText = uniqueUsernameGenerator.generateUsername();
+    // console.log(newAboutText);
+
+    await page.locator('textarea[name="about"]').fill(newAboutText);
+    await page.getByRole('button', { name: 'update' }).click();
+
+    return newAboutText;
+}
+
+// Log in with bad credentials
+export async function loginBadCredentialsHN(page) {
+    const loginForm = page.locator('form', {
+        has: page.getByRole('button', { name: 'login' }),
+    });
+
+    await loginForm.locator('input[name="acct"]').fill("invalidusername");
+    await loginForm.locator('input[name="pw"]').fill("password")
+
+    await loginForm.getByRole('button', { name: 'login' }).click();
 }
